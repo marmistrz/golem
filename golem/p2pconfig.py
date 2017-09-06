a6f32eda8f01a0695cf8abb5db9731b960820fb8
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2017-09-06 12:47:49 +0200

    New seed

--- golem/p2pconfig.py
+++ golem/p2pconfig.py
@@ -3,7 +3,7 @@ p2pconfig = {
         "listen_host": "0.0.0.0",
         "listen_port": 20171,
         "bootstrap_nodes": [
-		    b"enode://6f050c7361f539a3539a7cb5998a8186bc2b8fc60f6bf23a2db486854c47ddf919e98c2edd39b7031ed95bbf409ccbc4e0c32ffa60b9eea0f225a601bc186e39@94.23.17.170:20171"]
+		    b"enode://a9ac7cb82f08929e3253372b0c7fab7589fd37c9ee5cc2ad1cc9da75ffa8b78398450aa5a25f1836d4984700570f69aa8356a94ca9bd55c5d226d051c5bd6742@94.23.17.170:20171"]
      },
      "p2p": {
         "listen_host": "0.0.0.0",
